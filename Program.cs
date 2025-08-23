db3de6fbfd0ae1d58e1f7cb9a5d8732f8a7a3447
higor.losilva <higor.losilva@OSA0716338W11-1>
2025-08-23 10:52:09 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+{
+    // Esse programa calcula a média de três notas e exibe o resultado.
+    Console.WriteLine("\t Calculando a média de três notas:\n\t");
+            double nota1 = 8.5;
+            double nota2 = 7.2;
+            double nota3 = 9.0;
+
+
+    double media = (nota1 + nota2 + nota3) / 3;
+    Console.WriteLine("\n\t A média das notas é: " + media);
+
+
+}
